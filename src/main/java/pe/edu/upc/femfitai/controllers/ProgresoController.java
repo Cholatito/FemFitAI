2156392a567c15b42d189b0f1fdc9bfc84d65f12
Pebru18 <sleguiasobia@gmail.com>
2026-09-24 00:03:55 -0500

    Se me olvidó referenciar el dto

--- src/main/java/pe/edu/upc/femfitai/controllers/ProgresoController.java
+++ src/main/java/pe/edu/upc/femfitai/controllers/ProgresoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import pe.edu.upc.femfitai.dtos.ProgresoDTO;
 import pe.edu.upc.femfitai.services.interfaces.IProgresoService;
 
 @RestController
